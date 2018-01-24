a9eaa04c7418e42ce755ef44cfb455400a3f5a37
Yordan Ivanov <yordan.ivanov@scalefocus.com>
2018-01-24 07:02:45 +0200

    Just another commit.

--- BaseballClientGames/Properties/Resources.Designer.cs
+++ BaseballClientGames/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace BaseballClientGames.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Loading_icon {
-            get {
-                object obj = ResourceManager.GetObject("Loading_icon", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
